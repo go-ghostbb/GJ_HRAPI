abe8424671a8d027be21e53df5f20942dd55e367
go-ghostbb <mucgll0328@icloud.com>
2024-05-24 08:43:49 +0800

    fix: 修正註解

--- script/procedure/P_C_CheckInStatusUpdateTime.sql
+++ script/procedure/P_C_CheckInStatusUpdateTime.sql
@@ -12,8 +12,7 @@ begin
 
     if (@isWork = 1)
     begin
-        -- 如果沒更新到
-        -- 抓取最接近的上班的時間
+        -- 上班
         update check_in_status
         set work_check_in = @datetime
         where -- 抓取最接近的上班的時間
@@ -22,23 +21,21 @@ begin
                   join work_shift i_w on (i_c.work_shift_id = i_w.id)
                   where employee_id = (select id from employee where card_number = @card_number and deleted_at is null) and
                         i_c.deleted_at is null
-                  order by abs(datediff(second, @datetime, dbo.FN_Convert_Datetime(i_c.date, i_w.work_start))))
+                  order by abs(datediff(second, @datetime, dbo.FN_Convert_Datetime(i_c.date, i_w.work_start))));
     end
     else
     begin
-        -- 如果沒更新到
-        -- 抓取最接近的上班的時間
+        -- 下班
         update check_in_status
         set off_work_check_in = @datetime
-        where -- 抓取最接近的上班的時間
+        where -- 抓取最接近的下班的時間
             id = (select top 1 i_c.id
                   from check_in_status i_c
                   join work_shift i_w on (i_c.work_shift_id = i_w.id)
                   where employee_id = (select id from employee where card_number = @card_number and deleted_at is null) and
                        i_c.deleted_at is null
-                  order by abs(datediff(second, @datetime, dbo.FN_Convert_Datetime(iif(i_w.work_start > i_w.work_end, dateadd(day, 1, i_c.date), i_c.date), i_w.work_end))))
-
-    end
+                  order by abs(datediff(second, @datetime, dbo.FN_Convert_Datetime(iif(i_w.work_start > i_w.work_end, dateadd(day, 1, i_c.date), i_c.date), i_w.work_end))));
+    end;
     
     declare @date date = convert(date, @datetime);
     declare @start_date date = dateadd(day, -1, @date);
